320d5a56457af956c80380353b3deaecbb2a8639
Brad Langel <b.a.langel@gmail.com>
2018-02-16 14:31:50 -0500

    Factors out protocol parsing into Base module
    Allows us to have a high-level send/io interface without also containing
    the protocol parsing functions contained in Metric, Event, and Service
    check.

---
+++ dogstatsd.mli
@@ -0,0 +1,140 @@
+module Base : sig
+  module Tag : sig
+    type t = string * string (** (tag-name, value) *)
+  end
+
+  (** https://docs.datadoghq.com/developers/dogstatsd/#metrics *)
+  module Metric : sig
+    module Counter : sig
+      type t =
+        [ `Increment
+        | `Decrement
+        | `Value of int ]
+    end
+
+    type typ =
+      [ `Counter of Counter.t
+      | `Gauge of float
+      | `Timer of float
+      | `Histogram of int
+      | `Set of string ]
+
+    type t =
+      { metric_name : string
+      ; metric : typ
+      ; sample_rate : float option
+      ; tags : Tag.t list }
+  end
+
+  (** https://docs.datadoghq.com/developers/dogstatsd/#service-checks *)
+  module ServiceCheck : sig
+    type status =
+      [ `Ok
+      | `Warning
+      | `Critical
+      | `Unknown ]
+
+    type t =
+      { name : string
+      ; status : status
+      ; timestamp : int option
+      ; hostname : string option
+      ; tags : Tag.t list
+      ; message: string option }
+  end
+
+  (** https://docs.datadoghq.com/developers/dogstatsd/#events *)
+  module Event : sig
+    type priority = [ `Normal | `Low ]
+
+    type alert =
+      [ `Error
+      | `Warning
+      | `Info
+      | `Success ]
+
+    type t =
+      { title : string
+      ; text : string
+      ; timestamp : int option
+      ; hostname : string option
+      ; aggregation_key : string option
+      ; priority : priority option (* defaults to Normal *)
+      ; source_type_name : string option
+      ; alert_type : alert option (* defaults to Info *)
+      ; tags: Tag.t list }
+  end
+end
+
+(* TODO: replace IO with {Async, Lwt, Sync} *)
+module IO : sig
+
+  module Metric : sig
+    (** A couple of examples of how to send metrics:
+
+        * Async.Metric.send
+            ~tags:["tag1", "val1"; "tag2","val2"]
+            ~sample_rate:0.1
+            (`Counter `Increment) ["application.page_views"]
+        * Async.Metric.send
+            (`Set "some-user-id") ["application.user_ids"]
+
+    *)
+
+    val t_send : Base.Metric.t -> unit
+    val send
+      : ?tags:Base.Tag.t list
+      -> ?sample_rate:float
+      -> Base.Metric.typ
+      -> string list
+      -> unit
+  end
+
+  module ServiceCheck : sig
+    (** An example of how to send Service Checks:
+
+        ```
+        Async.ServiceCheck.send
+          ~tags:["tag1", "val1"; "tag2","val2"]
+          ~message:"Redis connection timed out after 10s"
+          `Critical
+          "mywebsite.can_connect_redis"
+        ```
+    *)
+
+    val t_send : Base.ServiceCheck.t -> unit
+    val send
+      : ?tags:Base.Tag.t list
+      -> ?message:string
+      -> ?hostname:string
+      -> ?timestamp:int
+      -> Base.ServiceCheck.status
+      -> string
+      -> unit
+  end
+
+  module Event : sig
+    (** An example of how to send Events:
+
+        ```
+        Async.Event.send
+          ~tags:["tag1", "val1"; "tag2","val2"]
+          ~alert_type:`Error
+          ~priority:`Low
+          ~title:"Page render error!"
+          ~text:"Some error message"
+        ```
+    *)
+    val t_send : Base.Event.t -> unit
+    val send
+      : ?tags:Base.Tag.t list
+      -> ?hostname:string
+      -> ?timestamp:int
+      -> ?aggregation_key:string
+      -> ?source_type_name:string
+      -> ?alert_type:Base.Event.alert
+      -> title:string
+      -> text:string
+      -> unit
+  end
+end
