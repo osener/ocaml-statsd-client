6b16130668c4de52f5b0277061dbf53dd62f368e
Brad Langel <b.a.langel@gmail.com>
2018-03-20 14:27:10 -0400

    Removes async from backwards compatible package

--- statsd-client/statsd_client.ml
+++ statsd-client/statsd_client.ml
@@ -1,4 +1,3 @@
 
 module Sync = Statsd_client_sync.T
 module Lwt = Statsd_client_lwt.T
-module Async = Statsd_client_async.T
--- statsd-client/statsd_client.mli
+++ statsd-client/statsd_client.mli
@@ -1,8 +1,7 @@
 (** Send statistics to the stats daemon over UDP
 
-    Synchronous (Sync) and two asynchronous (Lwt & Async) versions are provided.
+    Synchronous (Sync) and an asynchronous (Lwt) version are provided.
     Module aliasing is occuring here for backwards compatibility.
 *)
 module Sync = Statsd_client_sync.T
 module Lwt = Statsd_client_lwt.T
-module Async = Statsd_client_async.T
