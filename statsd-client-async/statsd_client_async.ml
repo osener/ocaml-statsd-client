768eb6821ef02e2d86ced7f98c68bbfbb0ed3d5f
Brad Langel <b.a.langel@gmail.com>
2018-03-22 09:57:49 -0400

    Adds Dogstatsd module to statsd-client-async lib

--- statsd-client-async/statsd_client_async.ml
+++ statsd-client-async/statsd_client_async.ml
@@ -1,44 +1,47 @@
 include Statsd_client_core.Config
-include Statsd_client_core.Make (
-  struct
-    open Core
-    open Async
+module T =  struct
+  open Core
+  open Async
 
-    let ipaddr () = !Statsd_client_core.Config.ipaddr
-    let port () = !Statsd_client_core.Config.port
+  let ipaddr () = !Statsd_client_core.Config.ipaddr
+  let port () = !Statsd_client_core.Config.port
 
-    type 'a _r = 'a Deferred.t
-    let ( >>= ) = ( >>= )
-    let return = return
-    let catch f error =
-      try_with f >>= function
-      | Ok d -> return d
-      | Error e -> error e
-    let list_iter f l = Deferred.List.iter ~f l
-    let close fd = Unix.close fd
+  type 'a _r = 'a Deferred.t
+  let ( >>= ) = ( >>= )
+  let return = return
+  let catch f error =
+    try_with f >>= function
+    | Ok d -> return d
+    | Error e -> error e
+  let list_iter f l = Deferred.List.iter ~f l
+  let close fd = Unix.close fd
 
-    type file_descr = Async_unix.Fd.t
+  type file_descr = Async_unix.Fd.t
 
-    let socket domain typ ip_protocol_num =
-      let socket_type =
-        match domain, typ, ip_protocol_num with
-        | Unix.PF_INET, Unix.SOCK_DGRAM, 17 (* UDP *) -> Socket.Type.udp
-        | Unix.PF_INET, Unix.SOCK_STREAM, 6 (* TCP *) -> Socket.Type.tcp
-        | _ -> failwith "Protocol not recognized"
-      in
-      Socket.fd (Socket.create socket_type)
+  let socket domain typ ip_protocol_num =
+    let socket_type =
+      match domain, typ, ip_protocol_num with
+      | Unix.PF_INET, Unix.SOCK_DGRAM, 17 (* UDP *) -> Socket.Type.udp
+      | Unix.PF_INET, Unix.SOCK_STREAM, 6 (* TCP *) -> Socket.Type.tcp
+      | _ -> failwith "Protocol not recognized"
+    in
+    Socket.fd (Socket.create socket_type)
 
-    let inet_addr_exn = function
-      | Unix.ADDR_INET (inet_addr, port) ->
-        Socket.Address.Inet.create inet_addr ~port
-      | Unix.ADDR_UNIX _ -> failwith "Unix domain addresses not supported"
+  let inet_addr_exn = function
+    | Unix.ADDR_INET (inet_addr, port) ->
+      Socket.Address.Inet.create inet_addr ~port
+    | Unix.ADDR_UNIX _ -> failwith "Unix domain addresses not supported"
 
-    let sendto fd msg offset msg_length flags socket_address =
-      match Udp.sendto () with
-      | Error e -> return (-1)
-      | Ok send ->
-        let buf = msg |> Iobuf.of_string |> Iobuf.read_only in
-        let inet_addr = inet_addr_exn socket_address in
-        send fd buf inet_addr
-        >>| fun () -> msg_length
-  end)
+  let sendto fd msg offset msg_length flags socket_address =
+    match Udp.sendto () with
+    | Error e -> return (-1)
+    | Ok send ->
+      let buf = msg |> Iobuf.of_string |> Iobuf.read_only in
+      let inet_addr = inet_addr_exn socket_address in
+      send fd buf inet_addr
+      >>| fun () -> msg_length
+end
+
+include Statsd_client_core.Make(T)
+
+module Dogstatsd = Dogstatsd.Make(T)
