6ac0884d2902069208135f4651e1bf47b83371f8
Brad Langel <b.a.langel@gmail.com>
2018-04-30 18:33:34 -0400

    Documentation about configuration between modules

--- statsd-client/statsd_client.mli
+++ statsd-client/statsd_client.mli
@@ -5,6 +5,10 @@
 *)
 
 include Statsd_client_core.C
+(** For backwards compatiblity reasons, the config is shared between
+    the Sync and Lwt modules. After this backwards compatible
+    module is deprecated we will no longer be supporting this shared
+    configuration behavior as it is less than ideal. *)
 module Sync = Statsd_client_sync
 module Lwt = Statsd_client_lwt
 [@@deprecated "Use [statsd-client-lwt] opam package for Lwt support"]
